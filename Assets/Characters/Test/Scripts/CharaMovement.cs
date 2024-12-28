4ac6a4a7b050299bd58cd6d6a238290a53ec4710
je-lam <jemian.lam@gmail.com>
2024-12-27 22:33:27 -0800

    Updated camera, hierarchy, and implemented ground profiles

--- Assets/Characters/Test/Scripts/CharaMovement.cs
+++ Assets/Characters/Test/Scripts/CharaMovement.cs
@@ -2,23 +2,79 @@ using UnityEngine;
 
 public class CharaMovement : MonoBehaviour
 {
-    const float DEF_SPEED = 20f;
-    const float DASH_SPEED = 80f;
+    private Rigidbody2D rb;
+    const float DEF_SPEED = 2f;
+    const float DASH_SPEED = 5f;
+
+    const string POLARITY = "SOUTH";
+
+    CircleCollider2D magneticField;
+
+    float currSpeed;
 
     float inputX;
     float inputY;
+    // -1 means pressing DOWN, 1 means pressing UP
+
+    public bool holdDownForMagnet; //this should later be handled by game manager
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         print("hello world");
+        currSpeed = DASH_SPEED;
+        magneticField = GetComponentInChildren<CircleCollider2D>();
+        holdDownForMagnet = true;
     }
 
     void Update()
     {
+        UpdateMoveInputs();
+        Vector2 moveDirection = new Vector2(inputX * currSpeed, 0);
+        if (inputX != 0)
+        {
+            MoveHoriz(moveDirection);
+        }
+
+        HandleMagnet();
+
+    }
+
+    public void UpdateMoveInputs()
+    {
         inputX = Input.GetAxis("Horizontal");
         inputY = Input.GetAxis("Vertical");
-        if (inputY != 1)
+    }
+
+    void MoveHoriz(Vector2 dir)
+    {
+        rb.linearVelocityX = dir.x * currSpeed;
+    }
+
+    void HandleMagnet()
+    {
+        if (holdDownForMagnet == true)
         {
-            print("Pressing DOWN key");
+            if (Input.GetKey(KeyCode.C))
+            {
+                magneticField.enabled = true;
+            }
+            else
+            {
+                if (magneticField.enabled == true)
+                {
+                    magneticField.enabled = false;
+                }
+            }
+            return;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                magneticField.enabled = !magneticField.enabled;
+                print("toggling magnet");
+            }
+
         }
     }
 }
