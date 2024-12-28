c662ef356475a85a37be8ce2bd05b5535a27317b
J <136391978+je-lam@users.noreply.github.com>
2024-12-27 17:57:18 -0800

    script changes?

--- Assets/Characters/Test/Scripts/CharaMovement.cs
+++ Assets/Characters/Test/Scripts/CharaMovement.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class CharaMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    const float DEF_SPEED = 20f;
+    const float DASH_SPEED = 80f;
+
+    float inputX;
+    float inputY;
     void Start()
     {
-        
+        print("hello world");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+        if (inputY != 1)
+        {
+            print("Pressing DOWN key");
+        }
     }
 }
