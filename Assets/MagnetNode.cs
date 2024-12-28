df698547e0c69cb7b95f1662536f2499d8dc90c8
je-lam <jemian.lam@gmail.com>
2024-12-28 13:37:04 -0800

    Minor scaling improvements

--- Assets/MagnetNode.cs
+++ Assets/MagnetNode.cs
@@ -6,10 +6,13 @@ public class MagnetNode : MonoBehaviour
 
     PointEffector2D magField;
     SpriteRenderer nodeSpriteRenderer;
+
+    UnityEngine.Color originalColor;
     void Start()
     {
         magField = GetComponent<PointEffector2D>();
         nodeSpriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = nodeSpriteRenderer.color;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
