4ce92753fbf6b94ac570ff0b5cf3da3c9eaa9245
al371160 <88258682+al371160@users.noreply.github.com>
2024-12-30 00:52:35 -0800

    tutorial level started
    just a commit to be safe

--- Assets/FinaTerrain/Scripts/FinalTerrain.cs
+++ Assets/FinaTerrain/Scripts/FinalTerrain.cs
@@ -1,14 +1,12 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer),typeof(EdgeCollider2D))]
-
-public class FinalTerrain : MonoBehaviour {
-
-    
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(EdgeCollider2D))]
+public class FinalTerrain : MonoBehaviour
+{
     Mesh mesh;
     EdgeCollider2D coll;
     public LineRenderer lr;
-    [Range(0,15000)]
+    [Range(0, 15000)]
     public int Xsize;
     public int Ysize;
 
@@ -19,17 +17,15 @@ public class FinalTerrain : MonoBehaviour {
 
     int _seed;
 
-    public float LineOffset = -0.25f; 
-   
-	void Awake ()
+    public float LineOffset = -0.25f;
+
+    void Awake()
     {
         Generate();
     }
 
-    
-    public void Generate ()
+    public void Generate()
     {
-
         mesh = new Mesh();
         coll = GetComponent<EdgeCollider2D>();
         lr = GetComponent<LineRenderer>();
@@ -37,19 +33,55 @@ public class FinalTerrain : MonoBehaviour {
 
         if (seed == 0)
         {
-            _seed = Random.Range(0,999);
+            _seed = Random.Range(0, 999);
         }
         else
         {
             _seed = seed;
         }
 
+        Vector3[] vertices = GenerateTerrain.Vertices(Xsize, Ysize, frequency, Amplitude, _seed);
+        mesh.vertices = vertices;
+        mesh.triangles = GenerateTerrain.Trinagles(Xsize, Ysize);
+        mesh.RecalculateNormals(); // Optional: Ensures lighting works properly
 
+        coll.points = GenerateTerrain.Collider(mesh.vertices, Xsize, Ysize);
+        UpdateColliderPosition();
 
+        TerrainRender.GenerateLineRenderer(mesh.vertices, Ysize, ref lr, LineOffset);
+        UpdateLineRendererPosition();
+    }
 
-        mesh.vertices = GenerateTerrain.Vertices(Xsize,Ysize,frequency,Amplitude,_seed);
-        mesh.triangles = GenerateTerrain.Trinagles(Xsize,Ysize);
-        coll.points = GenerateTerrain.Collider(mesh.vertices,Xsize,Ysize);
-        TerrainRender.GenerateLineRenderer(mesh.vertices,Ysize,ref lr,LineOffset);
-	}
+    void UpdateColliderPosition()
+    {
+        // Adjust collider points based on GameObject's position
+        Vector2 offset = transform.position;
+        Vector2[] colliderPoints = coll.points;
+        for (int i = 0; i < colliderPoints.Length; i++)
+        {
+            colliderPoints[i] += offset;
+        }
+        coll.points = colliderPoints;
+    }
+
+    void UpdateLineRendererPosition()
+    {
+        // Adjust line renderer to follow GameObject's position if necessary
+        if (lr.useWorldSpace)
+        {
+            for (int i = 0; i < lr.positionCount; i++)
+            {
+                lr.SetPosition(i, lr.GetPosition(i) + transform.position);
+            }
+        }
+    }
+
+    void OnValidate()
+    {
+        // Ensure Generate() runs when parameters change in the Editor
+        if (Application.isPlaying)
+        {
+            Generate();
+        }
+    }
 }
