6ccc2c00b6a3960f6d593311a85728c58216f465
je-lam <jemian.lam@gmail.com>
2024-12-30 02:32:09 -0800

    Actually got the balancing anim working

--- Assets/PengBalancer2.cs
+++ Assets/PengBalancer2.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PengBalancer2 : MonoBehaviour
@@ -7,19 +8,42 @@ public class PengBalancer2 : MonoBehaviour
 
     private Transform cachedTransform;
     private Vector3 cachedPosition;
+    Animator pengAnimator;
+    private string currState;
+    Rigidbody2D rb;
+    Vector3 worldVelocity;
+    Vector3 previousPosition;
+
+    int framesBetweenWpUpdate = 12;
+    int currFramesBetween = 0;
 
     void Start()
     {
+
+        pengAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         cachedTransform = GetComponent<Transform>();
         if (track)
         {
             cachedPosition = track.position;
         }
+
+
+
     }
 
     void Update()
     {
         TrackPosition();
+        AnimUpdate();
+
+        if (currFramesBetween >= framesBetweenWpUpdate)
+        {
+            worldVelocity.x = (transform.position.x - previousPosition.x) / Time.deltaTime;
+            previousPosition = transform.position;
+            currFramesBetween = 0;
+        }
+        currFramesBetween++;
     }
 
     void TrackPosition()
@@ -29,5 +53,28 @@ public class PengBalancer2 : MonoBehaviour
             cachedPosition = track.position + new Vector3(0, 1.9f, 0);
             transform.position = cachedPosition + new Vector3(0, 1.9f, 0);
         }
+    }
+
+    void AnimUpdate()
+    {
+        if (math.abs(worldVelocity.x) > 6)
+        {
+            ChangeAnimState("Balancing");
+            print(math.abs(worldVelocity.x));
+        }
+        else
+        {
+            ChangeAnimState("Idle_Balance");
+        }
+    }
+
+    void ChangeAnimState(string newState)
+    {
+        if (currState == newState)
+        {
+            return;
+        }
+        pengAnimator.Play(newState);
+        currState = newState;
     }
 }
