c03652ae51be087b80973ecf550355b8391fe75c
al371160 <88258682+al371160@users.noreply.github.com>
2024-12-29 18:08:36 -0800

    terrain generation (optional)
    added another unity package I found online. https://www.youtube.com/watch?v=4x2xItGU0ec (good materials as well)

---
+++ Assets/Editor/SimpleTerrainEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FinalTerrain))]
+public class SimpleTerrainEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("ReGenerate"))
+        {
+            FinalTerrain finalterrain = (FinalTerrain)target;
+            finalterrain.Generate();
+        }
+    }
+
+}
